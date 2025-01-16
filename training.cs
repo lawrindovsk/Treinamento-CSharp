468ee75e2c110c595e1565d92b75438dc63729cd
lawrindovsk <gustavo.laurindo.santos@gmail.com>
2025-01-15 23:49:00 -0300

    first commit

---
+++ training.cs
@@ -0,0 +1,50 @@
+using System;
+using Arrays;
+using Data;
+using NumerosMoedas;
+
+namespace Treinamento
+{
+
+    public class Menu()
+    {
+        public static void Treinamento()
+        {
+            Console.WriteLine("Treinamento .NET");
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("1 - Datas ");
+            Console.WriteLine("2 - Moedas (validações, deciamais, culture e etc.)");
+            Console.WriteLine("3 - Arrays ");
+            Console.WriteLine("# -");
+            Console.WriteLine("# -");
+            Console.WriteLine("# -");
+            Console.WriteLine("# -");
+
+#pragma warning disable CS8604 // Possível argumento de referência nula.
+            short choose = short.Parse(Console.ReadLine());
+#pragma warning restore CS8604 // Possível argumento de referência nula.
+
+            switch (choose)
+            {
+                case 1:
+                    Datas.MostrarData();
+                    break;
+                case 2:
+                    Moedas.NumerosMoedas();
+                    Moedas.maisMoedas();
+                    break;
+                case 3:
+                    Listas.arryasList();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    Menu.Treinamento();
+                    break;
+            }//
+        }//
+    }//
+}//
